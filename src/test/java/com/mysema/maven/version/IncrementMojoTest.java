5edf2c183e225ec2a683a99c79eb0ff0b054a4df
Timo Westkämper <timo.westkamper@mysema.com>
2012-09-23 11:27:30 +0300

    added tests

--- src/test/java/com/mysema/maven/version/IncrementMojoTest.java
+++ src/test/java/com/mysema/maven/version/IncrementMojoTest.java
@@ -1,13 +1,12 @@
 package com.mysema.maven.version;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.base.Charsets;
@@ -15,18 +14,13 @@ import com.google.common.io.Files;
 
 public class IncrementMojoTest {
 
-    private File sourcePom, targetPom;
-    
-    @Before
-    public void setUp() throws IOException {
-        sourcePom = new File("src/test/resources/project-to-test/pom.xml");
-        targetPom = new File("target/project-to-test1/pom.xml");        
-        targetPom.getParentFile().mkdir();
-        Files.copy(sourcePom, targetPom);        
-    }
-    
     @Test
-    public void Execute() throws MojoExecutionException, MojoFailureException, IOException {        
+    public void Execute() throws MojoExecutionException, MojoFailureException, IOException {
+        File sourcePom = new File("src/test/resources/project-to-test/pom.xml");
+        File targetPom = new File("target/project-to-test1/pom.xml");        
+        targetPom.getParentFile().mkdir();
+        Files.copy(sourcePom, targetPom);
+        
         IncrementMojo mojo = new IncrementMojo();
         mojo.setBasedir(new File("target/project-to-test1"));
         mojo.execute();
--- src/test/java/com/mysema/maven/version/SetVersionMojoTest.java
+++ src/test/java/com/mysema/maven/version/SetVersionMojoTest.java
@@ -1,34 +1,42 @@
 package com.mysema.maven.version;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.*;
+import com.google.common.io.Files;
 
 public class SetVersionMojoTest {
 
-    private File sourcePom, targetPom;
-    
-    @Before
-    public void setUp() throws IOException {
-        sourcePom = new File("src/test/resources/project-to-test/pom.xml");
-        targetPom = new File("target/project-to-test2/pom.xml");        
+    @Test
+    public void Execute() throws MojoExecutionException, MojoFailureException, IOException {
+        File sourcePom = new File("src/test/resources/project-to-test/pom.xml");
+        File targetPom = new File("target/project-to-test2/pom.xml");        
         targetPom.getParentFile().mkdir();
         Files.copy(sourcePom, targetPom);        
+        
+        SetVersionMojo mojo = new SetVersionMojo();
+        mojo.setBasedir(new File("target/project-to-test2"));
+        System.setProperty("version", "2.0.1");
+        mojo.execute();
+        assertTrue(Files.toString(targetPom, Charsets.UTF_8).contains("<version>2.0.1</version>"));
     }
     
     @Test
-    public void Execute() throws MojoExecutionException, MojoFailureException, IOException {        
+    public void ExecuteModule() throws IOException, MojoExecutionException, MojoFailureException {
+        File sourcePom = new File("src/test/resources/module-to-test/pom.xml");
+        File targetPom = new File("target/module-to-test2/pom.xml");        
+        targetPom.getParentFile().mkdir();
+        Files.copy(sourcePom, targetPom);        
+        
         SetVersionMojo mojo = new SetVersionMojo();
-        mojo.setBasedir(new File("target/project-to-test2"));
+        mojo.setBasedir(new File("target/module-to-test2"));
         System.setProperty("version", "2.0.1");
         mojo.execute();
         assertTrue(Files.toString(targetPom, Charsets.UTF_8).contains("<version>2.0.1</version>"));
